582598845362d8b90780407b0c748728ab7272a7
StormJose <ricardo23feitosa@gmail.com>
2023-04-04 12:37:47 -0300

    pushing calculator base project

---
+++ Calculator/js/script.js
@@ -0,0 +1,151 @@
+const numBtns = document.querySelectorAll('.num')
+const operationBtns = document.querySelectorAll('.opr')
+const equalBtn = document.querySelector('.equal')
+const delBtn = document.querySelector('.del')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+
+
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+  }
+
+
+clear() {
+  this.currentOperand = ''
+  this.previousOperand = ''
+  this.operation = undefined
+  
+}
+
+delete() {
+  this.currentOperand = this.currentOperand.toString().slice(0, -1)
+}
+
+getDisplayNumber(number) {
+  
+  const stringNumber = number.toString()
+  const integerDigits = parseFloat(stringNumber.split('.')[0])
+  const decimalDigits = stringNumber.split('.')[1]
+  let integerDisplay
+  if(isNaN(integerDigits)) {
+    integerDisplay = ''
+  } else {
+    integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+    
+  }
+  if (decimalDigits != null) {
+    return `${integerDisplay}.${decimalDigits}`
+  }
+  else {
+    return integerDisplay
+  }
+
+  
+    
+}
+
+appendNumber(number) {
+  if (number === '.' && this.currentOperand.includes('.')) return
+  this.currentOperand = this.currentOperand.toString() + number.toString()
+}
+
+chooseOperation(operation){
+  if (this.currentOperand === '') return
+  if(this.previousOperand !== '') {
+    this.compute()
+  }
+  this.operation = operation
+  this.previousOperand = this.currentOperand
+  this.currentOperand = ''
+}
+
+compute() {
+  let computation
+  const prev = parseFloat(this.previousOperand)
+  const current = parseFloat(this.currentOperand)
+  
+   
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+      case '*':
+      computation = prev * current
+      break
+      case '+':
+      computation = prev + current
+      break
+      case '-':
+      computation = prev - current
+      break
+      case '÷':    
+      computation = prev / current
+      break
+    } 
+      this.currentOperand = computation
+      this.operation = undefined
+      this.previousOperand = ''
+
+}
+
+updateDisplay() {
+  this.currentOperandTextElement.innerText = this.currentOperand
+  if (this.operation != null) {
+    this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+  } else {
+    this.previousOperandTextElement.innerText = ''
+  }
+
+}
+
+
+}
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+numBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+  
+})
+
+
+operationBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+equalBtn.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
+
+
+
+delBtn.addEventListener('click', button => {
+  calculator.delete()
+  calculator.updateDisplay()
+})
+
+// keyboard input
+document.addEventListener('keydown', (e) => {
+  const numbers = ['0','1', '2', '3', '4', '5', '6', '8', '9']
+  const operations = ['*', '+', '/', '-']
+  const name = e.key;
+  const code = e.code;
+  console.log(name)
+  if(numbers.includes(name)) {
+    calculator.appendNumber(name)
+    calculator.updateDisplay()
+  } else{
+    calculator.compute()
+    calculator.updateDisplay()
+  }
+})
